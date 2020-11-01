77976fcfc174ad1d17ca6014bca8deb41799d1f4
donggeol lee <donggeollee@donggeolui-MacBookPro.local>
2020-11-01 18:37:58 +0900

    application-*.yml

--- src/main/java/com/boot/test/service/BooterUserDetailsService.java
+++ src/main/java/com/boot/test/service/BooterUserDetailsService.java
@@ -3,7 +3,6 @@ package com.boot.test.service;
 import com.boot.test.api.domain.Member;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -16,10 +15,10 @@ import java.util.List;
 public class BooterUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        PasswordEncoder bencoder = new BCryptPasswordEncoder();
+        PasswordEncoder bEncoder = new BCryptPasswordEncoder();
         SimpleGrantedAuthority authority = new SimpleGrantedAuthority("ROLE_USER");
         List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
         authorities.add(authority);
-        return new Member("donggeol",  bencoder.encode("yi109304"), authorities);
+        return new Member("donggeol",  bEncoder.encode("yi109304"), authorities);
     }
 }
