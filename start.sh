98367a2a384afcf48f1eb40f4ec2c3d80e14c035
donggeollee <42575543+donggeollee@users.noreply.github.com>
2020-11-03 01:42:27 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,11 @@
 echo "recognize start.sh" &&
+echo "recognize start.sh" &&
+echo "recognize start.sh" &&
+echo "recognize start.sh" &&
+echo "recognize start.sh" &&
+echo "recognize start.sh" &&
+echo "recognize start.sh" &&
+echo "recognize start.sh" &&
+echo "recognize start.sh" &&
 docker build -t booter-image . && 
 docker run -d -p 8080:8080 --name booter booter-image
